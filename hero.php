5378c9c05ed59702bf8e919c1f0c173294ee8710
Vincent Z <iamkaruto@gmail.com>
2012-02-25 22:41:26 -0800

    The initial version of top-featured-set is finally done.

--- hero.php
+++ hero.php
@@ -2,60 +2,91 @@
 <div class="hero-set">
 
 	<div id="feature0" class="feature-box hero-feature feature-big feature0 grid_4 column">
+		<a href="http://mangatalk.net/topic/love/">
 		<div class="image-window shadowbox rainbow1" style="height:350px; width:326px;">
 			<div class="feature-meta"><h2 class="feature-title">
-			漫画手札年度漫画大赏专题
+			井上雄彦『浪客行』、三月神之归来
 			</h2></div>
-			<img height="350" src="http://i.imgur.com/mR0FE.jpg" style="positon:absolute; top:0px;" />
+			<img height="350" src="http://i.imgur.com/W4YGG.jpg" style="positon:absolute; top:0px;"/>
 			
 		</div><!-- .image-window -->
+		</a>
 	</div><!-- #feature0 -->
+	
+	
 	
 	<div class="grid_8 column">
 		<div class="clearfix">
+		
+	
 			<div class="feature-box hero-feature feature-small grid_4 column">
+				<a href="http://mangatalk.net/2011/tokyo-love-story-rika/">
+				
 				<div class="image-window shadowbox rainbow2" style="height:175px; width:326px;">
 					<div class="feature-meta"><h2 class="feature-title">
-					这是我自幼憧憬追逐着的梦：高桥留美子意大利专访
+					TOKYO LOVE STORY：写给赤名莉香
 					</h2></div>
-					<img src="http://i.imgur.com/ePLIe.jpg" style="positon:absolute; top:0px; left:0px" width="340"/>
+					<img src="http://i.imgur.com/Vy55y.jpg" style="positon:absolute; top:0px; left:0px"/>
 
 				</div><!-- .image-window -->
+				
+				</a>
 			</div><!-- .feature-box -->
 			
+			
+			
+			
 			<div class="feature-box hero-feature feature-small grid_4 column">
+				<a href="http://mangatalk.net/topic/interview">
+				
 				<div class="image-window shadowbox rainbow1" style="height:175px; width:326px;">
 					<div class="feature-meta"><h2 class="feature-title">
-					德岛签售会现场作画实录：森薰、入江亚季
+					2011 年度漫画大赏：『宇宙兄弟』
 					</h2></div>
-					<img src="http://i.imgur.com/nYE6A.jpg" style="positon:absolute; top:0px; left:0px" width="340"/>
-
+					<img src="http://i.imgur.com/UydmA.jpg" style="positon:absolute; top:0px; left:0px"/>
 				</div><!-- .image-window -->
+				
+				</a>
 			</div><!-- .feature-box -->
+			
+			
 			
 		</div><!-- .clearfix -->
 		
 
+		
 		<div class="clearfix">
+		
 			<div class="feature-box hero-feature feature-small grid_4 column">
+				<a href="http://mangatalk.net/2012/2011-manga-space-bros/">
+			
 				<div class="image-window shadowbox rainbow3" style="height:175px; width:326px;">
 					<div class="feature-meta"><h2 class="feature-title">
-					诗意之美：『尼金斯基的寓言』中的舞蹈考据
+					追溯『无限之住人』：专访沙村广明
 					</h2></div>
-					<img src="http://i.imgur.com/cQxxg.jpg" style="positon:absolute; top:0px; left:0px" width="340"/>
+					<img src="http://i.imgur.com/cQxxg.jpg" style="positon:absolute; top:0px; left:0px"/>
 				</div><!-- .image-window -->
+				
+				</a>
 			</div><!-- .feature-box -->
+			
 			
 			<div class="feature-box hero-feature feature-small grid_4 column">
+				<a href="http://mangatalk.net/2011/i-love-emma/">
+				
 				<div class="image-window shadowbox rainbow4" style="height:175px; width:326px;">
 					<div class="feature-meta"><h2 class="feature-title">
-					追溯『无限之住人』：专访沙村广明
+					致心底最柔软的地方：浅谈森薰，与她的『爱玛』
 					</h2></div>							
-					<img src="http://i.imgur.com/cQxxg.jpg" style="positon:absolute; top:0px; left:0px" width="340"/>
+					<img src="http://i.imgur.com/nYE6A.jpg" style="positon:absolute; top:0px; left:0px"/>
 				</div><!-- .image-window -->
+				
+				</a>
 			</div><!-- .feature-box -->
 			
+			
 		</div><!-- .clearfix -->					
+		
 		
 	</div><!-- .grid_8 -->
 	
