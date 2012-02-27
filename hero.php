176d46430488d7be7213ef84998d5280230594d7
Vincent Z <iamkaruto@gmail.com>
2012-02-26 20:30:51 -0800

    Fixed featured set redirection error, added user-friendly prompt for profile pages in dashboard.

--- hero.php
+++ hero.php
@@ -2,7 +2,7 @@
 <div class="hero-set">
 
 	<div id="feature0" class="feature-box hero-feature feature-big feature0 grid_4 column">
-		<a href="http://mangatalk.net/topic/love/">
+		<a href="#">
 		<div class="image-window shadowbox rainbow1" style="height:350px; width:326px;">
 			<div class="feature-meta"><h2 class="feature-title">
 			井上雄彦『浪客行』、三月神之归来
@@ -37,7 +37,7 @@
 			
 			
 			<div class="feature-box hero-feature feature-small grid_4 column">
-				<a href="http://mangatalk.net/topic/interview">
+				<a href="http://mangatalk.net/2012/2011-manga-space-bros/">
 				
 				<div class="image-window shadowbox rainbow1" style="height:175px; width:326px;">
 					<div class="feature-meta"><h2 class="feature-title">
@@ -58,7 +58,7 @@
 		<div class="clearfix">
 		
 			<div class="feature-box hero-feature feature-small grid_4 column">
-				<a href="http://mangatalk.net/2012/2011-manga-space-bros/">
+				<a href="#">
 			
 				<div class="image-window shadowbox rainbow3" style="height:175px; width:326px;">
 					<div class="feature-meta"><h2 class="feature-title">
